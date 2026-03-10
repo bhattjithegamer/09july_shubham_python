b216b75b7f6e2167777b5fa8efec5871029414bc
bhatt_ji <sbha6482@gmail.com>
2026-03-10 15:00:43 +0530

    commit

--- vertual_Enviourment/ecommerce/frontend/next.config.ts
+++ vertual_Enviourment/ecommerce/frontend/next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* images સેટિંગ્સ - જેથી Render પરથી ફોટા દેખાય */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ecommerce-backend-pgyl.onrender.com',
+      },
+    ],
+  },
+  /* headers સેટિંગ્સ - જેથી Google Login નું પોપ-અપ બ્લોક ના થાય */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
